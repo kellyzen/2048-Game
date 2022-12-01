3660db096c37a1fc83f74a5ca7e267266c1db53e
kellyzen <02tankelly@gmail.com>
2022-12-01 23:36:29 +0800

    Added javadoc comments in IBackgroundScene interface and ITheme interface

--- src/main/java/com/example/game/theme/IBackgroundScene.java
+++ src/main/java/com/example/game/theme/IBackgroundScene.java
@@ -1,5 +1,21 @@
 package com.example.game.theme;
 
+/**
+ *
+ * BackgroundScene interface.
+ *
+ * @author Kelly Kai Ling Tan
+ * @version Dec 16, 2022
+ * @author Coursework: COMP2042
+ *
+ */
 public interface IBackgroundScene {
+    /**
+     *
+     * Change background scene colour for all scene when a theme is selected.
+     *
+     * @param theme theme name
+     *
+     */
     void changeBackgroundScene(String theme);
 }
--- src/main/java/com/example/game/theme/ITheme.java
+++ src/main/java/com/example/game/theme/ITheme.java
@@ -2,9 +2,50 @@ package com.example.game.theme;
 
 import javafx.scene.control.ChoiceBox;
 
+/**
+ *
+ * Theme interface.
+ *
+ * @author Kelly Kai Ling Tan
+ * @version Dec 16, 2022
+ * @author Coursework: COMP2042
+ *
+ */
 public interface ITheme {
+    /**
+     *
+     * Change theme css path based on player's choice from dropdown.
+     *
+     * @param newTheme name of new theme
+     *
+     */
     void newTheme(String newTheme);
+    /**
+     *
+     * Add selected theme css to the current root.
+     *
+     * @param node theme choice box value
+     * @param themeFile file name of the theme selected
+     *
+     */
     void addTheme(ChoiceBox<String> node, String themeFile);
+    /**
+     *
+     * Remove all theme css on the current root when new theme is selected.
+     * Call removeTheme().
+     *
+     * @param node theme choice box value
+     *
+     */
     void removeAllTheme(ChoiceBox<String> node);
+    /**
+     *
+     * Change overall theme for all scene.
+     * Call addTheme() and newTheme();
+     *
+     * @param themeName name of theme selected
+     * @param node theme choice box value
+     *
+     */
     void changeTheme(String themeName, ChoiceBox<String> node);
 }
