74d38acf8e3854183a88e7cbe103709462d57c67
kellyzen <02tankelly@gmail.com>
2022-12-01 23:02:00 +0800

    Added javadoc comments in Menu package

--- src/main/java/com/example/game/menu/menuController.java
+++ src/main/java/com/example/game/menu/menuController.java
@@ -1,6 +1,7 @@
 package com.example.game.menu;
 
 import com.example.game.components.dialogComponent.QuitDialog;
+import com.example.game.game.GameMode;
 import com.example.game.game.GameScene;
 import com.example.game.theme.Theme;
 import javafx.event.ActionEvent;
@@ -8,13 +9,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -22,49 +21,98 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+/**
+ *
+ * MenuController class.
+ * Controller class for menu.fxml
+ *
+ * @author Kelly Kai Ling Tan
+ * @version Dec 16, 2022
+ * @author Coursework: COMP2042
+ *
+ */
 public class menuController implements Initializable {
     static final int WIDTH = 900;
     static final int HEIGHT = 600;
-
     Stage primaryStage = Menu.getPrimaryStage();
     private Group gameRoot = new Group();
     private Scene gameScene = new Scene(gameRoot, WIDTH, HEIGHT);
-    Theme newTheme = new Theme();
-
-    private Color backgroundScene;
+    private static Color backgroundScene;
     @FXML Label highScoreLabel;
     @FXML Label usernameLabel;
     @FXML private ChoiceBox<String> menuChoiceBox;
+    @FXML ImageView imageView;
+    @FXML Button nextButton;
+    @FXML Button prevButton;
+    @FXML Label modeLabel;
+    GameMode gameMode = new GameMode();
+    private final String[] theme = Theme.getThemeNames();
 
-    private final String[] theme = {"Dark", "Light", "Fantasy", "Nymph", "Default"};
-
+    /**
+     *
+     * Set player's username text.
+     *
+     * @param text username
+     */
     public void setUsernameLabel(String text) {
         usernameLabel.setText(text);
     }
-
+    /**
+     *
+     * Set player's high score label.
+     *
+     * @param score player's high score
+     */
     public void setHighScoreLabel(Long score) {
         highScoreLabel.setText(String.valueOf(score));
     }
-
+    /**
+     *
+     * Set game scene.
+     *
+     * @param gameScene game scene
+     */
     public void setGameScene(Scene gameScene) {
         this.gameScene = gameScene;
     }
-
+    /**
+     *
+     * Get game scene.
+     *
+     * @return gameScene game scene
+     */
     public Scene getGameScene() {
         return gameScene;
     }
-
+    /**
+     *
+     * Set game root.
+     *
+     * @param gameRoot game scene
+     */
     public void setGameRoot(Group gameRoot) {
         this.gameRoot = gameRoot;
     }
-
-    public void setBackgroundScene(Color backgroundScene) {
-        this.backgroundScene = backgroundScene;
+    /**
+     *
+     * Set background scene colour.
+     *
+     * @param backgroundScene background scene
+     */
+    public static void setBackgroundScene(Color backgroundScene) {
+        menuController.backgroundScene = backgroundScene;
     }
 
+    /**
+     *
+     * Switch to Game Scene when Start Game button clicked.
+     *
+     */
     public void switchToGame() {
+        Theme newTheme = new Theme();
         String themePath = Theme.getTheme();
-        changeBackgroundScene();
+        newTheme.changeBackgroundScene();
+
         //set root and scene for game
         Group gameRoot = new Group();
         setGameRoot(gameRoot);
@@ -72,17 +120,18 @@ public class menuController implements Initializable {
         Scene gameScene = new Scene(gameRoot, WIDTH, HEIGHT, backgroundScene);
         setGameScene(gameScene);
         primaryStage.setScene(gameScene);
+
         //set root and scene for end game
         Group endgameRoot = new Group();
         Scene endGameScene = new Scene(endgameRoot, WIDTH, HEIGHT, backgroundScene);
         endgameRoot.getStylesheets().add(getClass().getResource(themePath).toString());
 
         //reset game  mode
-        changeImage();
+        gameMode.changeMode(imageView, modeLabel);
 
         //set grid number (n) and startGame method in GameScene
         GameScene game = new GameScene();
-        GameScene.setN(n);
+        GameScene.setN(GameMode.getN());
         game.startGame(gameScene, gameRoot, primaryStage, endGameScene, endgameRoot);
 
         //set title and full screen
@@ -90,6 +139,12 @@ public class menuController implements Initializable {
         primaryStage.setFullScreen(true);
         primaryStage.show();
     }
+
+    /**
+     *
+     * Switch to Rank Scene when Rank button clicked.
+     *
+     */
     public void switchToRank() throws IOException {
         String themePath = Theme.getTheme();
         Parent rankRoot = FXMLLoader.load(getClass().getResource("/com/example/game/GUI/rank.fxml"));
@@ -101,10 +156,20 @@ public class menuController implements Initializable {
         primaryStage.show();
     }
 
+    /**
+     *
+     * Switch to Game Scene when Quit Game button clicked.
+     *
+     */
     public void quitGame() {
         new QuitDialog();
     }
 
+    /**
+     *
+     * Initialize actions for menuChoiceBox to change theme colour.
+     *
+     */
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         menuChoiceBox.getItems().addAll(theme);
@@ -112,108 +177,40 @@ public class menuController implements Initializable {
 
     }
 
+    /**
+     *
+     * Change theme when menuChoiceBox value is changed.
+     *
+     */
     public void changeTheme(ActionEvent event) {
+        Theme newTheme = new Theme();
         gameScene = getGameScene();
-
         String theme = menuChoiceBox.getValue();
-        menuChoiceBox.getScene().getRoot().getStylesheets().remove(getClass().getResource("/com/example/game/styling/dark.css").toString());
-        menuChoiceBox.getScene().getRoot().getStylesheets().remove(getClass().getResource("/com/example/game/styling/light.css").toString());
-        menuChoiceBox.getScene().getRoot().getStylesheets().remove(getClass().getResource("/com/example/game/styling/fantasy.css").toString());
-        menuChoiceBox.getScene().getRoot().getStylesheets().remove(getClass().getResource("/com/example/game/styling/nymph.css").toString());
-        menuChoiceBox.getScene().getRoot().getStylesheets().remove(getClass().getResource("/com/example/game/styling/default.css").toString());
-
-        switch (theme) {
-            case "Dark" -> {
-                menuChoiceBox.getScene().getRoot().getStylesheets().add(getClass().getResource("/com/example/game/styling/dark.css").toString());
-                newTheme.newTheme("Dark");
-            }
-            case "Light" -> {
-                menuChoiceBox.getScene().getRoot().getStylesheets().add(getClass().getResource("/com/example/game/styling/light.css").toString());
-                newTheme.newTheme("Light");
-            }
-            case "Fantasy" -> {
-                menuChoiceBox.getScene().getRoot().getStylesheets().add(getClass().getResource("/com/example/game/styling/fantasy.css").toString());
-                newTheme.newTheme("Fantasy");
-            }
-            case "Nymph" -> {
-                menuChoiceBox.getScene().getRoot().getStylesheets().add(getClass().getResource("/com/example/game/styling/nymph.css").toString());
-                newTheme.newTheme("Nymph");
-            }
-            case "Default" -> {
-                menuChoiceBox.getScene().getRoot().getStylesheets().add(getClass().getResource("/com/example/game/styling/default.css").toString());
-                newTheme.newTheme("Default");
-            }
-        }
-
-        changeBackgroundScene();
+        newTheme.removeAllTheme(menuChoiceBox);
+        newTheme.changeTheme(theme, menuChoiceBox);
+        newTheme.changeBackgroundScene();
     }
 
-    private void changeBackgroundScene() {
-        switch(Theme.getTheme()){
-            case "/com/example/game/styling/dark.css" -> {
-                setBackgroundScene(Color.web("#373e43"));
-            }
-            case "/com/example/game/styling/light.css" -> {
-                setBackgroundScene(Color.web("#eee"));
-            }
-            case "/com/example/game/styling/fantasy.css" -> {
-                setBackgroundScene(Color.web("#D6B9F3"));
-            }
-            case "/com/example/game/styling/nymph.css" -> {
-                setBackgroundScene(Color.web("#5c9aaf"));
-            }
-            case "/com/example/game/styling/default.css" -> {
-                setBackgroundScene(Color.rgb(189, 177, 92));
-            }
-        }
-    }
-
-    private int mode = 1;
-    private static int n = 4;
-    private final int numOfMode = 4;
-    @FXML ImageView imageView;
-    @FXML Button nextButton;
-    @FXML Button prevButton;
-    @FXML Label modeLabel;
-
-    Image tinyMode = new Image(getClass().getResourceAsStream("/com/example/game/images/tiny.png"));
-    Image classicMode = new Image(getClass().getResourceAsStream("/com/example/game/images/classic.png"));
-    Image largeMode = new Image(getClass().getResourceAsStream("/com/example/game/images/large.png"));
-    Image hugeMode = new Image(getClass().getResourceAsStream("/com/example/game/images/huge.png"));
-
+    /**
+     *
+     * Switch to the next game mode.
+     * Change to next mode's image and label.
+     *
+     */
     public void nextMode() {
-        mode = (mode + 1) % numOfMode;
-        changeImage();
+        gameMode.nextMode();
+        gameMode.changeMode(imageView, modeLabel);
     }
 
+    /**
+     *
+     * Switch to the previous game mode.
+     * Change to previous mode's image and label.
+     *
+     */
     public void prevMode() {
-        mode = (mode - 1) % numOfMode;
-        changeImage();
-    }
-
-    private void changeImage() {
-        switch (mode) {
-            case 0 -> {
-                imageView.setImage(tinyMode);
-                modeLabel.setText("Tiny (3x3)");
-                n = 3;
-            }
-            case 1, -3 -> {
-                imageView.setImage(classicMode);
-                modeLabel.setText("Classic (4x4)");
-                n = 4;
-            }
-            case 2, -2 -> {
-                imageView.setImage(largeMode);
-                modeLabel.setText("Large (5x5)");
-                n = 5;
-            }
-            case 3, -1 -> {
-                imageView.setImage(hugeMode);
-                modeLabel.setText("Huge (6x6)");
-                n = 6;
-            }
-        }
+        gameMode.prevMode();
+        gameMode.changeMode(imageView, modeLabel);
     }
 }
 
